f73ddab69026b988e7ae7c2983b377a1cf900310
Alemph <jlsangregorio@up.edu.ph>
2024-05-09 05:15:32 +0800

    cleared

--- src/TempInputField.js
+++
@@ -1,9 +0,0 @@
-export default function TempInputField(props){
-    return(
-        <>
-            <legend>Temperature Converter</legend>
-            <input type="number" onChange={e => props.func(e.target.value)}
-            value={props.value}/> {props.unitofmeasure}
-        </>
-    );
-}    
